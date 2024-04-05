c3e8d3fca328e3d0a59a3219af037f389922a13a
laudoluk <Laudo-Finda-Pedro.Lukoki@hsrw.org>
2024-04-05 02:58:21 +0200

    Create LaudoMobile1.ino

---
+++ Code/LaudoMobile1/LaudoMobile1.ino
@@ -0,0 +1,14 @@
+Laudo Lukoki
+/*Read and took information from:
+https://electrosome.com/interfacing-l298n-motor-driver-arduino-uno/
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
