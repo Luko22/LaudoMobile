8089935c3ca19d385273a5542c1e0b3e6c397d94
laudoluk <Laudo-Finda-Pedro.Lukoki@hsrw.org>
2024-04-09 23:36:07 +0200

    Update LaudoMobile1.ino

--- Code/LaudoMobile1/LaudoMobile1.ino
+++ Code/LaudoMobile1/LaudoMobile1.ino
@@ -1,19 +1,24 @@
-Laudo Lukoki
+//Laudo Lukoki
 /*Read and took information from:
 https://electrosome.com/interfacing-l298n-motor-driver-arduino-uno/
 */
 
-// Motor A connections
+/* Motor A connections: 
+in1 and in2 pins are used to control the direction of Motor A
+connected to pin 13, pin 12*/
 int enA = 9;
 int in1 = 13;
 int in2 = 12;
-// Motor B connections
+/* Motor B connections: 
+in3 and in4 pins are used to control the direction of Motor B
+connected to pin 11, pin 10*/
 int enB = 3;
 int in3 = 11;
 int in4 = 10;
 
 //only runs once
 void setup() {
+
     pinMode(enA, OUTPUT);
     pinMode(enB, OUTPUT);
     pinMode(in1, OUTPUT);
@@ -28,11 +33,62 @@ void setup() {
 }
 
 void loop() {
-    directionControl();
-    delay(1000);
-    speedControl();
-    delay(1000);
+
+    analogWrite(enA, 100);
+    analogWrite(enB, 100);
+ 
+  // motor A CW ^ (forward)
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+
+  // motor B CW ^ (forward)
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+
+    delay(2000);
+
+//  // Turn off motors
+//     digitalWrite(in1, LOW);
+//     digitalWrite(in2, LOW);
+//     digitalWrite(in3, LOW);
+//     digitalWrite(in4, LOW);
+//     delay(2000);
+
+//   // motor A CCW  (backwards)
+//     digitalWrite(in1, HIGH);
+//     digitalWrite(in2, LOW);
+
+//   // motor B CCW (backwards)
+//     digitalWrite(in3, LOW);
+//     digitalWrite(in4, HIGH);
+
+//     delay(1000);
+
+
+
+ // // motor A CW ^
+  //   digitalWrite(in1, LOW);
+  //   digitalWrite(in2, HIGH);
+
+  // // motor B CCW
+  //   digitalWrite(in3, HIGH);
+  //   digitalWrite(in4, LOW);
+    
+
+    // Turn off motors
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    delay(2000);
+
+
+    // directionControl();
+    // delay(1000);
+    // speedControl();
+    // delay(1000);
 }
+
 
 //function
 void directionControl() {
@@ -60,6 +116,9 @@ void directionControl() {
     digitalWrite(in3, LOW);
     digitalWrite(in4, LOW);
 }
+
+
+
 void speedControl() {
     // Turn on motors
     digitalWrite(in1, LOW);
